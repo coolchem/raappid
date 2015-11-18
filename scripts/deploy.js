58fefbd8c6dc11a8ce51e2b59ba845aa58fbcef7
varunreddy <varunreddy.nalagatla@davita.com>
2015-11-17 23:30:44 -0700

    updating deploy and release scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,2 +1,12 @@
 
 var util = require('./util');
+
+util.exec("npm publish",function(err){
+    if(err)
+    {
+        console.log(err);
+        process.exit(1);
+    }
+
+    process.exit(0);
+});
--- scripts/release.js
+++ scripts/release.js
@@ -1,3 +1,32 @@
 
 var util = require('./util');
+var argv = require('minimist')(process.argv.slice(2));
+
+if(argv._ && argv._.length > 0) //look release build
+{
+
+    var cmd = "npm version " + argv._[0];
+
+    console.log(cmd);
+    util.series(["npm test",
+        "npm run build-release",
+        cmd,
+        "git push && git push --tags"],function(err){
+        if(err)
+        {
+            console.log(err);
+            process.exit(1);
+        }
+
+        process.exit(0);
+    });
+
+}
+else
+{
+    console.log("\n Cannot Recognize the type of release. Please see instructions below");
+    console.log('\n Options:\n\n raapid-release [release-type]' +
+        '      --- Give type of release:  major | minor | patch | premajor | preminor | prepatch | prerelease\n'
+    );
+}
 
