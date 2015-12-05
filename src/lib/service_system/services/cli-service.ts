372c2cb6727c9bf5a3f0a9cb4da97a0853aaa6d9
varunreddy <varunreddy.nalagatla@davita.com>
2015-12-05 11:04:36 -0700

    adding cli service for command line support

---
+++ src/lib/service_system/services/cli-service.ts
@@ -0,0 +1,75 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+
+
+import chalk = require('chalk');
+
+
+export const ERROR_UNSUPPORTED_COLOR:string = `Error logging message: The color sent to log the message unsupported
+                                               Please send one of the following colors: red,blue,green,yellow`;
+
+export function confirm(question:string):Promise<boolean>
+{
+    question += '[y/n]';
+
+    var promise = askInput(question);
+
+    return promise.then((answer)=>{
+        if (answer.match(/\b(no|n)\b/i))
+            return false;
+        else if (answer.match(/\b(yes|y\b)/i))
+            return true;
+        else
+            return confirm(question);
+    });
+}
+
+export function askInput(message:string):Promise<string>
+{
+
+    return new Promise(function (resolve) {
+        console.log(message+":");
+        process.stdin.setEncoding('utf8');
+        var inputVal = '';
+        process.stdin.on('data', function (data) {
+            var lastChar = data.substr(data.length - 1, 1);
+            if (lastChar === '\n' || lastChar === '\r' || lastChar === '\u0004') {
+                process.stdin.removeAllListeners('data');
+                inputVal += data.substr(0, data.length - 1);
+                resolve(inputVal.trim());
+            }
+            inputVal += data;
+        });
+    });
+}
+
+export function log(value:string,color?:string):void
+{
+    if(color && color != "")
+    {
+        color = color.toLowerCase();
+
+        if(color !== "red" && color !== "blue" && color !== "green" && color !== "yellow")
+        {
+            throw new Error(ERROR_UNSUPPORTED_COLOR);
+        }
+        value = chalk[color](value);
+    }
+
+    console.log(value);
+
+}
+
+export function warn(message:string):void
+{
+    log(message,"yellow");
+}
+
+export function logError(message:string):void
+{
+    log(message,"red");
+}
+
+export function logSuccess(message:string):void
+{
+    log(message,"green");
+}
---
+++ test/service_system/services/cli-service.spec.ts
@@ -0,0 +1,246 @@
+/// <reference path="../../../src/typings/tsd.d.ts" />
+
+import chai = require('chai');
+import sinon = require('sinon');
+import chalk = require('chalk');
+
+import cliService =require("../../../src/lib/service_system/services/cli-service");
+
+chai.use(require("sinon-chai"));
+
+var stdin = require('mock-stdin').stdin();
+
+describe('cli-service Test cases', () => {
+
+    var expect = chai.expect;
+    var logSpy = sinon.spy(console,"log");
+
+    describe('log', () => {
+
+        it('should call the console.log with the message', function(done) {
+
+            cliService.log("message");
+            expect(logSpy).to.have.been.calledWith("message");
+            done();
+
+        });
+
+        it('should not throw error if color is null or undefined or empty string', function(done) {
+
+            cliService.log("message1","");
+
+            expect(logSpy).to.have.been.calledWith("message1");
+            cliService.log("message2");
+
+            expect(logSpy).to.have.been.calledWith("message2");
+            cliService.log("message3",null);
+
+            expect(logSpy).to.have.been.calledWith("message3");
+
+            done();
+
+        });
+
+        it('should throw error is unsupported color is given', function(done) {
+
+            var throws = function(){
+                cliService.log("message","asdaad");
+            };
+
+
+            expect(throws).to.throw(cliService.ERROR_UNSUPPORTED_COLOR);
+            done();
+
+        });
+
+        it('should not throw error if supported color is given', function(done) {
+            var color:string = "red";
+            var throws = function(){
+                cliService.log("message",color);
+            };
+
+            expect(throws).to.not.throw(cliService.ERROR_UNSUPPORTED_COLOR);
+
+            color = "blue";
+            expect(throws).to.not.throw(cliService.ERROR_UNSUPPORTED_COLOR);
+
+            color = "green";
+            expect(throws).to.not.throw(cliService.ERROR_UNSUPPORTED_COLOR);
+
+            color = "Green";
+            expect(throws).to.not.throw(cliService.ERROR_UNSUPPORTED_COLOR);
+
+            color = "yellow";
+            expect(throws).to.not.throw(cliService.ERROR_UNSUPPORTED_COLOR);
+            done();
+
+        });
+
+        it('should apply the color if sent to log the message with', function(done) {
+
+            cliService.log("message","red");
+
+            expect(logSpy).to.have.been.calledWith(chalk.red("message"));
+            done()
+
+        });
+
+    });
+
+    describe('warn', () => {
+
+        it('should log message with color yellow', function(done) {
+
+            cliService.warn("message");
+            expect(logSpy).to.have.been.calledWith(chalk.yellow("message"));
+            done();
+
+        });
+
+    });
+
+    describe('logError', () => {
+
+        it('should log message in red', function(done) {
+
+            cliService.logError("message");
+            expect(logSpy).to.have.been.calledWith(chalk.red("message"));
+            done();
+        });
+
+    });
+
+    describe('logSuccess', () => {
+
+        it('should log message in green', function(done) {
+
+            cliService.logError("message");
+            expect(logSpy).to.have.been.calledWith(chalk.green("message"));
+            done();
+        });
+
+    });
+
+
+    describe('askInput', () => {
+
+        beforeEach(function () {
+            stdin = require('mock-stdin').stdin();
+        });
+
+        afterEach(()=>{
+            stdin.end();
+            stdin.reset();
+        });
+
+        it('should ask user for input', function(done) {
+
+            process.nextTick(function mockResponse() {
+                stdin.send('i am fine\n');
+            });
+
+            cliService.askInput("Hello how are u?").then((input)=>{
+
+                expect(logSpy).to.have.been.calledWith("Hello how are u?:");
+                expect(input).to.equal("i am fine");
+                done();
+            })
+
+        });
+
+    });
+
+    describe('confirm', () => {
+
+        beforeEach(function () {
+            stdin = require('mock-stdin').stdin();
+        });
+
+        afterEach(()=>{
+            stdin.end();
+            stdin.reset();
+        });
+
+        it('should ask user to give yes or no answer', function(done) {
+
+            cliService.confirm("Are we there yet?").then(()=>{
+
+            });
+            expect(logSpy).to.have.been.calledWith("Are we there yet?[y/n]:");
+            done();
+        });
+
+
+        it('should re-ask the question if the answer is not yes/no or y/n', function(done) {
+
+            process.nextTick(function mockResponse() {
+                stdin.send('i do not know\n');
+            });
+
+            cliService.confirm("Are we there yet yet?").then(()=>{});
+
+            expect(logSpy).to.have.been.calledWithExactly("Are we there yet yet?[y/n]:");
+            done();
+        });
+
+
+        it('should return true if the answer is yes or y', function(done) {
+            process.nextTick(function mockResponse() {
+                stdin.send('yes\n');
+            });
+
+            cliService.confirm("Are we there yet?").then((answer)=>{
+
+                expect(answer).to.equal(true);
+
+                process.nextTick(function mockResponse() {
+                    stdin.send('y\n');
+                });
+
+                cliService.confirm("Are we there yet?").then((answer)=>{
+
+                    expect(answer).to.equal(true);
+
+                    done();
+
+                })
+
+            })
+        });
+
+        it('should return false if the answer is no or n', function(done) {
+            process.nextTick(function mockResponse() {
+                stdin.send('no\n');
+            });
+
+            cliService.confirm("Are we there yet?").then((answer)=>{
+
+                expect(answer).to.equal(false);
+
+                process.nextTick(function mockResponse() {
+                    stdin.send('n\n');
+                });
+
+                cliService.confirm("Are we there yet?").then((answer)=>{
+
+                    expect(answer).to.equal(false);
+
+                    done();
+
+                })
+
+            })
+        });
+
+    });
+
+
+
+
+
+
+});
+
+
+
+
