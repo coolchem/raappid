df52c7ca2871c5057128f8b36c1d8b7a904097a8
varunreddy <varun8686@gmail.com>
2015-12-10 01:29:33 -0700

    implementing repo-service

---
+++ src/lib/service_system/services/repo-service.ts
@@ -0,0 +1,67 @@
+
+import shell = require("../utils/shell-util")
+var which = require("which");
+var GitHubApi = require("github");
+export var github = new GitHubApi({
+    // required
+    version: "3.0.0"
+});
+
+export function validateGit():boolean
+{
+    try
+    {
+        return which("git");
+    }
+    catch (e)
+    {
+        return false;
+    }
+
+}
+
+export function initializeGit(projectDirectory:string):Promise<boolean>
+{
+    return shell.exec("git init",projectDirectory);
+}
+
+
+export function addAllFilesAndCommit(commitMessage:string,projectDirectory:string):Promise<boolean>
+{
+    return shell.series(["git add -A",'git commit -m ' + '"'+commitMessage +'"'],projectDirectory);
+}
+
+
+export function cloneGitRepository(username:string,repoName:string,dirToCloneInto:string):Promise<boolean>
+{
+
+    var cmd:string = "git clone git@github.com:" +username+"/"+repoName;
+    return shell.exec(cmd,dirToCloneInto);
+}
+
+
+export function createRemoteRepository(username:string,password:string,repoName:string):Promise<any>
+{
+
+
+    return new Promise((resolve,reject)=>{
+
+        github.authenticate({
+            type: "basic",
+            username: username,
+            password: password
+        });
+
+        github.repos.create({name:repoName},(error,result)=>{
+
+            if(error)
+            {
+                reject(error);
+                return;
+            }
+
+            resolve(result);
+        });
+    });
+}
+
---
+++ test/service_system/services/repo-service.spec.ts
@@ -0,0 +1,233 @@
+/// <reference path="../../../src/typings/tsd.d.ts" />
+
+import chai = require('chai');
+import sinon = require('sinon');
+import repoService = require("../../../src/lib/service_system/services/repo-service");
+import path = require("path");
+import fs = require("fs-extra");
+import shell = require("../../../src/lib/service_system/utils/shell-util");
+import SinonStub = Sinon.SinonStub;
+import SinonSpy = Sinon.SinonSpy;
+
+var which = require("which");
+
+chai.use(require("sinon-chai"));
+require('sinon-as-promised');
+
+
+describe('repo-service Test cases', () => {
+
+    var expect = chai.expect;
+
+    describe('validateGit', () => {
+
+        var whichStub:SinonStub;
+        beforeEach(()=>{
+            whichStub = sinon.stub(which,"sync");
+        });
+
+        afterEach(()=>{
+            whichStub.restore();
+        });
+
+
+        it('should return false if git not installed', function() {
+
+            whichStub.returns(new Error("no git"));
+            expect(repoService.validateGit()).to.be.false;
+        });
+
+
+        it('should return true is git installed', function() {
+
+            whichStub.returns(true);
+            expect(repoService.validateGit()).to.be.false;
+        });
+
+    });
+
+    describe('initializeGit', () => {
+
+        var tempProjectDir:string = path.resolve("./test/tempProject");
+
+        var shellSpy:SinonSpy;
+        beforeEach(()=>{
+            shellSpy = sinon.spy(shell,"exec");
+
+        });
+
+        afterEach(()=>{
+            shellSpy.restore();
+        });
+
+        it('should run git init in the project directory given', function(done) {
+
+
+            fs.mkdirsSync(tempProjectDir);
+            repoService.initializeGit(tempProjectDir).then((result)=>{
+
+                expect(shellSpy).calledWith("git init",tempProjectDir);
+                fs.readdir(process.cwd()+"/.git",(error)=>{
+
+                    if(error)
+                    {
+                        done(new Error("Git not initialized"));
+                    }
+                    else
+                    {
+                        done();
+                    }
+                    fs.removeSync(tempProjectDir);
+                });
+            },(error)=>{
+                done(error);
+                fs.removeSync(tempProjectDir);
+            });
+
+
+        });
+
+        it('should reject with error if there was issue with git init', function(done) {
+
+            shellSpy.restore();
+            var shellStub:any = sinon.stub(shell,"exec");
+            shellStub.rejects(new Error("yay"));
+
+            repoService.initializeGit(tempProjectDir).then(null,(error)=>{
+                expect(shellStub).calledWith("git init",tempProjectDir);
+                expect(error).to.be.instanceOf(Error);
+                shellStub.restore();
+                done();
+            });
+        });
+
+    });
+
+    describe('addAllFilesAndCommit', () => {
+
+        var shellStub;
+        beforeEach(()=>{
+            shellStub = sinon.stub(shell,"series");
+
+        });
+
+        afterEach(()=>{
+            shellStub.restore();
+        });
+
+
+        it('should run git add -a and git commit -m in series', function(done) {
+
+            shellStub.resolves(true);
+            repoService.addAllFilesAndCommit("testCommit","directoryPath").then(()=>{
+
+                expect(shellStub).to.have.been.calledWith(["git add -A",'git commit -m ' + '"'+"testCommit" +'"'],"directoryPath");
+                done();
+            });
+
+        });
+
+        it('should reject with error if there was any issue', function(done) {
+
+            shellStub.rejects(new Error("yay"));
+            repoService.addAllFilesAndCommit("testCommit","directoryPath").then(null,(error)=>{
+
+                expect(shellStub).to.have.been.calledWith(["git add -A",'git commit -m ' + '"'+"testCommit" +'"'],"directoryPath");
+                expect(error).to.be.instanceOf(Error);
+                expect(error.message).to.equal("yay");
+                done();
+            });
+
+
+        });
+    });
+
+    describe('cloneGitRepository', () => {
+
+        var shellStub;
+        beforeEach(()=>{
+            shellStub = sinon.stub(shell,"exec");
+
+        });
+
+        afterEach(()=>{
+            shellStub.restore();
+        });
+
+        it('should create appropriate remote url and call git clone', function(done) {
+
+
+            shellStub.resolves(true);
+
+            repoService.cloneGitRepository("test","test","testDir").then(()=>{
+
+                expect(shellStub).to.have.been.calledWith("git clone git@github.com:test/test","testDir");
+
+                done();
+
+
+            })
+        });
+
+        it('should reject with error if there was any error in git cloning', function(done) {
+
+
+            shellStub.rejects(new Error("yay"));
+
+            repoService.cloneGitRepository("test","test","testDir").then(null,(error)=>{
+
+                expect(shellStub).to.have.been.calledWith("git clone git@github.com:test/test","testDir");
+                expect(error).to.be.instanceOf(Error);
+                expect(error.message).to.equal("yay");
+                done();
+
+            })
+        });
+
+    });
+
+
+    describe('createRemoteRepository', () => {
+
+        var spy:SinonSpy;
+        var stub:SinonStub;
+        beforeEach(function () {
+            spy = sinon.spy(repoService.github, "authenticate");
+            stub = sinon.stub(repoService.github.repos, "create").yieldsTo("yay");        });
+        afterEach(function () {
+            spy.restore();
+            stub.restore();
+        });
+
+        it('should authenticate github, with the username and password given', function(done) {
+
+
+            repoService.createRemoteRepository("test","test","test");
+
+            expect(spy).to.have.been.calledWith({
+                type: "basic",
+                username: "test",
+                password: "test"
+            });
+            done();
+
+        });
+
+        it('should create remote repo with the given repo name', function(done) {
+
+            repoService.createRemoteRepository("test","test","test").then(()=>{});
+
+            expect(stub).to.have.been.calledWith({
+                name: "test"
+            });
+            done();
+        });
+
+    });
+
+
+});
+
+
+
+
